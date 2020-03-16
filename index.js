541147b226924572e5ce195963798ce3d7b5ab60
Ian Mancini <ianmethyst@gmail.com>
2020-03-16 12:07:42 -0300

    Add new API endpoints. Use path module to use absolute paths

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { spawnSync } = require('child_process');
+const path = require('path');
+const { sample, remove } = require('lodash');
+const fs = require('fs');
+const moment = require('moment-timezone');
 
 const app = express();
+
+app.use(fileUpload({
+	createParentPath: true,
+}));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const port = process.env.PORT || 3000;
 
@@ -14,62 +27,75 @@ const city = 'La Plata';
 const school = 'Bachillerato de Bellas Artes';
 const age = 17;
 
-app.use(fileUpload({
-  createParentPath: true,
-}));
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-
+const day = moment.tz("America/Argentina/Buenos_Aires").format("YYYY-MM-DD");
 
 app.post('/upload-photo', async (req, res) => {
-  try {
-    if (!req.files && !req.files.photo) {
-      res.send({
-        status: false,
-        message: 'No file uploaded',
-      });
-    } else {
-      const { photo } = req.files;
+	try {
+		if (!req.files && !req.files.photo) {
+			res.send({
+				status: false,
+				message: 'No file uploaded',
+			});
+		} else {
+			const { photo } = req.files;
 
-      const data = {
-        name: photo.name,
-        mimetype: photo.mimetype,
-        size: photo.size,
-      };
+			const data = {
+				name: photo.name,
+				mimetype: photo.mimetype,
+				size: photo.size,
+			};
 
-      // move photo to uploads directory
-      const filepath = `uploads/${photo.name}`;
-      await photo.mv(`./${filepath}`);
+			// move photo to uploads directory
+			const filepath = path.join(__dirname + `/uploads/${photo.name}`);
+			await photo.mv(filepath);
 
-      const python = spawnSync(
-        'bash',
-        ['-e', './runProcess.sh', `../${filepath}`, `${city}`, `${school}`, `${age}`]);
-      console.log("stdout: ",python.stdout.toString('utf8'));
-      console.log("stderr: ",python.stderr.toString('utf8'));
+			const python = spawnSync(
+				'bash',
+				['-e', path.join(__dirname + '/runProcess.sh'), filepath, `${city}`, `${school}`, `${age}`]);
+			console.log("stdout: ",python.stdout.toString('utf8'));
+			console.log("stderr: ",python.stderr.toString('utf8'));
 
-      if (python.status === 0) {
-        res.send({
-          status: true,
-          message: 'Photo uploaded and processed succesfully',
-          data,
-        });
-      } else {
-        res.status(500).send();
-      }
-    }
-  } catch (err) {
-    res.status(500).send(err);
-    throw err;
-  }
+			if (python.status === 0) {
+				res.send({
+					status: true,
+					message: 'Photo uploaded and processed succesfully',
+					data,
+				});
+			} else {
+				res.status(500).send();
+			}
+		}
+	} catch (err) {
+		res.status(500).send(err);
+		throw err;
+	}
 });
 
-// TODO: Remove this line. Only use for testing purposes
-// app.use(express.static('uploads'));
-app.use(express.static('./website/build'));
+let lastFile = undefined;
+
+app.get('/random', function(req, res) {
+	const files = fs.readdirSync(path.join(__dirname + `/process/processed/${day}`));
+	
+	if (lastFile) {
+		remove(files, f => ( f === lastFile ));
+		console.log(`removing ${lastFile}`);
+	}
+
+	const file = sample(files);
+	lastFile = file;
+
+	res.json({ url: `/processed/${file}` });
+
+});
+
+app.use('/processed', express.static(path.join(__dirname + `/process/processed/${day}`)));
+
+app.use(express.static(path.join(__dirname) + '/website/build'));
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/website/build'));
+});
+
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}.`);
+	console.log(`App is listening on port ${port}.`);
 });
--- runProcess.sh
+++ runProcess.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-cd process
+cd /home/pi/server/process
 pipenv run python3 main.py "$1" "$2" "$3" "$4"
 exit 0
