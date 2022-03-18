9bd71cc95c7b1a124b39027f846d8687a8ea6430
Ian Mancini <ianmethyst@gmail.com>
2022-03-18 01:52:52 -0300

    Fix typo in constants

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ const UPLOADS_DIR = path.join(STORAGE_DIR, 'uploads')
 const PROCESSED_IMAGES_DIR = path.join(STORAGE_DIR, 'processed')
 
 const PROCESS_BIN = path.join(__dirname, 'process', 'process')
-const WEBSITE_DIR = path.join(__dirname, 'webite', 'build')
+const WEBSITE_DIR = path.join(__dirname, 'website', 'build')
 
 module.exports = {
   STORAGE_DIR,
